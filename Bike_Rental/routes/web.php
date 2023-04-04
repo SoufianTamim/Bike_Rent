f7917cb318047d8f927df64b4bbf98a730465608
Soufian Tamim <soufiantamim22@gmail.com>
2023-04-04 14:55:40 +0000

    intial commit

--- Bike_Rental/routes/web.php
+++ Bike_Rental/routes/web.php
@@ -14,47 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
-
-Route::get('/welcome', function () {
-    return view('welcome');
-})->name('welcome');
-
-
 Route::get('/', function () {
-    return view('index');
-})->name('index');
-
-Route::get('/404', function () {
-    return view('404');
-})->name('404');
-
-Route::get('/about', function () {
-    return view('about');
-})->name('about');
-
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
-Route::get('/gallery', function () {
-    return view('gallery');
-})->name('gallery');
-
-Route::get('/bikes', function () {
-    return view('bikes');
-})->name('bikes');
-
-Route::get('/sbike', function () {
-    return view('single_bike');
-})->name('sbike');
-
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
